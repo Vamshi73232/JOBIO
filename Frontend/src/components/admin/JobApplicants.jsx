6cc04d2a9dc627f4cd1a06738cd5d443a6ec8f9b
Vamshi73232 <gvamshi73232@gmail.com>
2026-08-08 13:19:26 +0530

    fixing frontned errors to deploy on render

--- Frontend/src/components/admin/JobApplicants.jsx
+++ Frontend/src/components/admin/JobApplicants.jsx
@@ -1,39 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../shared/Navbar'
 import JobApplicantsTable from './JobApplicantsTable'
-import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { APPLICATION_API_END_POINT } from "@/utils/constant"
-import {setApplicants} from "@/redux/applicationSlice"
-import { useSelector } from 'react-redux'
+import { setApplicants } from "@/redux/applicationSlice"
+
 export default function JobApplicants() {
   const params = useParams()
   const dispatch = useDispatch()
-  const {applicants} = useSelector((store)=>store.application)
-  useEffect(()=>{
-    const fetchAllApplicants = async()=>{
-      try{
-        const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`,{withCredentials:true})
-        if (res.data.success){
+  const { applicants } = useSelector((store) => store.application)
+
+  useEffect(() => {
+    const fetchAllApplicants = async () => {
+      try {
+        axios.defaults.withCredentials = true;
+        const res = await axios.get(
+          `${APPLICATION_API_END_POINT}/${params.id}/applicants`,
+          { withCredentials: true }
+        )
+        if (res.data?.success) {
           dispatch(setApplicants(res.data.job))
         }
-      }catch(err){
+      } catch (err) {
         console.log(err)
       }
     }
-    fetchAllApplicants();
-  },[])
+    if (params.id) {
+      fetchAllApplicants();
+    }
+  }, [params.id, dispatch])
+
+  // Safely extract the applicants count without crashing
+  const applicantsCount = applicants?.applications?.length || 0;
+
   return (
     <div>
-        <Navbar/>
-        <div className="max-w-6xl mx-auto">
-            <h1 className="font-bold text-xl my-5">
-                Applicants : {applicants.applications .length}
-            </h1>
-            <JobApplicantsTable/>
-        </div>
+      <Navbar />
+      <div className="max-w-6xl mx-auto px-4">
+        <h1 className="font-bold text-xl my-5">
+          Applicants : ({applicantsCount})
+        </h1>
+        <JobApplicantsTable />
+      </div>
     </div>
   )
 }
