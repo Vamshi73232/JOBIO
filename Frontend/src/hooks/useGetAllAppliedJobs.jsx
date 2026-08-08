4f1c4b7a4508c918cd3d2b55245aa10bc8ebb0d7
Vamshi73232 <gvamshi73232@gmail.com>
2026-08-08 13:15:06 +0530

    fixing frontend errors to deploy on render

--- Frontend/src/hooks/useGetAllAppliedJobs.jsx
+++ Frontend/src/hooks/useGetAllAppliedJobs.jsx
@@ -1,35 +1,28 @@
-import { useEffect } from "react";
+import React from 'react'
 import axios from "axios";
-import { useDispatch } from "react-redux";
-import { setAllAppliedJobs } from "@/redux/jobSlice";
+import {useDispatch} from "react-redux"
+import {useEffect} from "react";
+import { setAllAppliedJobs } from '@/redux/jobSlice';
 import { APPLICATION_API_END_POINT } from "@/utils/constant";
-
-const useGetAllAppliedJobs = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const fetchAllAppliedJobs = async () => {
-      try {
-        axios.defaults.withCredentials = true;
-        const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {
-          withCredentials: true,
-        });
-
-        console.log("Applied Jobs API Response:", res.data);
-
-        if (res.data?.success) {
-          // Check both plural (applications) and singular (application) key returned by backend
-          const appliedJobsData =
-            res.data.applications || res.data.application || [];
-          dispatch(setAllAppliedJobs(appliedJobsData));
+const  useGetAllAppliedJobs = () => {
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        const fetchAllAppliedJobs = async ()=>{
+            try{
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`,{withCredentials:true})
+                console.log(res.data)
+                if (res.data.success){
+                    dispatch(setAllAppliedJobs(res.data.application))
+                }
+            }catch(err){
+                console.log(err)
+            }   
         }
-      } catch (err) {
-        console.log("Error fetching applied jobs:", err);
-      }
-    };
+        fetchAllAppliedJobs();
+    },[]);
+  return (
+    <div>useGetAllAppliedJobs</div>
+  )
+}
 
-    fetchAllAppliedJobs();
-  }, [dispatch]);
-};
-
-export default useGetAllAppliedJobs;
+export default useGetAllAppliedJobs
